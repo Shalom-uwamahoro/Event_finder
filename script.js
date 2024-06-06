db02e2f0979bfb17fe823d78233bf8530066b4de
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-06-06 17:09:59 +0300

    modified the search button

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const apiKey = 'xCVvAJSve3E08MZ6dNepSezkRNCFgOeU';
-
 const button = document.getElementById('searchButton');
 const eventsContainer = document.getElementById('eventsContainer');
-
 const getEvents = () => {
     const city = document.getElementById('locationInput').value;
     const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&city=${city}`;
-
     eventsContainer.innerHTML = '<div class="loading-message">Fetching events...</div>';
-
     fetch(url)
         .then(response => {
             if (!response.ok) {
@@ -23,10 +19,8 @@ const getEvents = () => {
                 events.forEach(event => {
                     const eventElement = document.createElement('div');
                     eventElement.classList.add('event');
-                    
                     const date = new Date(event.dates.start.dateTime).toLocaleString();
                     const imageUrl = event.images ? event.images[0].url : 'https://picsum.photos/200';
-
                     eventElement.innerHTML = `
                         <div class="event-image">
                             <img src="${imageUrl}" alt="${event.name}">
@@ -49,8 +43,32 @@ const getEvents = () => {
             eventsContainer.innerHTML = '<div class="loading-message">Failed to fetch events</div>';
         });
 };
-
 button.addEventListener('click', getEvents);
+
+// Our default events
+
+function displayAllEvents() {
+    const defaultCity = 'New York';
+    const url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&city=${defaultCity}`;
+    fetch(url)
+     .then(response => response.json())
+     .then(data => {
+        console.log(getEvents(data._embedded));
+      })
+     .catch(error => {
+        console.error('Check your internet connection:', error);
+      });
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+    displayAllEvents();
+  });
+
+  
+const cancelBtn= document.getElementById("cancel")
+cancelBtn.addEventListener("click", function(){
+    document.getElementById("locationInput").value = "";
+})
+
 
 
 
